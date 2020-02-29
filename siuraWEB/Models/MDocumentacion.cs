39a8c44e69c8dd743727ba15b1d3a918f4ad49ea
Trujix <manuel_trujillo@ucol.mx>
2020-02-29 01:28:59 -0600

    Actualizacion
    Valores Financieros primarios de Paciente

--- siuraWEB/Models/MDocumentacion.cs
+++ siuraWEB/Models/MDocumentacion.cs
@@ -36,6 +36,14 @@ namespace siuraWEB.Models
             public double MontoPagar { get; set; }
         }
 
+        // CLASE DE PAGOS PACIENTES
+        public class PacientePagos
+        {
+            public int IdFinanzas { get; set; }
+            public double MontoPago { get; set; }
+            public string FolRefDesc { get; set; }
+        }
+
         // ----------- FUNCIONES GENERALES -----------
 
         // FUNCION QUE GUARDA EL REGISTRO DEL PACIENTE [ PREVIO ] ( :: PACIENTES ::)
@@ -112,7 +120,7 @@ namespace siuraWEB.Models
             {
                 List<Dictionary<string, object>> ListaPacientesPagos = new List<Dictionary<string, object>>();
                 SQL.comandoSQLTrans("PacientesPagosPend");
-                SQL.commandoSQL = new SqlCommand("SELECT PR.*, PP.montototal FROM dbo.pacienteregistro PR JOIN dbo.pacienteregistrofinanzas PP ON PP.idpaciente = PR.id WHERE PR.idusuario = (SELECT id FROM dbo.usuarios WHERE tokenusuario = @TokenDATA) AND PR.estatus = 1", SQL.conSQL, SQL.transaccionSQL);
+                SQL.commandoSQL = new SqlCommand("SELECT PR.*, PP.id AS idFinanzas, PP.montototal FROM dbo.pacienteregistro PR JOIN dbo.pacienteregistrofinanzas PP ON PP.idpaciente = PR.id WHERE PR.idusuario = (SELECT id FROM dbo.usuarios WHERE tokenusuario = @TokenDATA) AND PR.estatus = 1", SQL.conSQL, SQL.transaccionSQL);
                 SQL.commandoSQL.Parameters.Add(new SqlParameter("@TokenDATA", SqlDbType.VarChar) { Value = tokenusuario });
                 using (var lector = SQL.commandoSQL.ExecuteReader())
                 {
@@ -120,6 +128,7 @@ namespace siuraWEB.Models
                     {
                         Dictionary<string, object> Paciente = new Dictionary<string, object>()
                         {
+                            { "IdFinanzas", int.Parse(lector["idFinanzas"].ToString()) },
                             { "IdPaciente", int.Parse(lector["id"].ToString()) },
                             { "Nombre", lector["nombre"].ToString() },
                             { "ApellidoP", lector["apellidopaterno"].ToString() },
@@ -146,19 +155,68 @@ namespace siuraWEB.Models
             }
         }
 
-        // FUNCION PROVISIONAL QUE PAGA PENDIENTE DEL PACIENTE
-        public string PagarPendientePaciente(int idpaciente, string tokenpaciente)
+        // FUNCION QUE DEVUELVE LA LISTA DE PAGOS DEL PACIENTE
+        public string ListaPagosPaciente(int idfinanzas, string tokenusuario)
         {
             try
             {
-                SQL.comandoSQLTrans("PacientePagoPend");
-                SQL.commandoSQL = new SqlCommand("UPDATE dbo.pacienteregistro SET estatus = 2 WHERE id = @IDPacienteParam AND idusuario = (SELECT id FROM dbo.usuarios WHERE tokenusuario = @TokenUsuarioParam)", SQL.conSQL, SQL.transaccionSQL);
-                SqlParameter[] pacientePagar =
+                SQL.comandoSQLTrans("PacientePagosLista");
+                List<Dictionary<string, object>> ListaPagos = new List<Dictionary<string, object>>();
+                SQL.commandoSQL = new SqlCommand("SELECT * FROM dbo.pacienteregistropagos WHERE idfinanzas = @IdFinanzasParam AND idusuario = (SELECT id FROM dbo.usuarios WHERE tokenusuario = @TokenDATA)", SQL.conSQL, SQL.transaccionSQL);
+                SqlParameter[] pagosPacienteFinanzas =
                 {
-                    new SqlParameter("@IDPacienteParam", SqlDbType.Int) { Value = idpaciente },
-                    new SqlParameter("@TokenUsuarioParam", SqlDbType.VarChar) { Value = tokenpaciente }
+                    new SqlParameter("@TokenDATA", SqlDbType.VarChar){Value = tokenusuario },
+                    new SqlParameter("@IdFinanzasParam", SqlDbType.Int){Value = idfinanzas }
                 };
-                SQL.commandoSQL.Parameters.AddRange(pacientePagar);
+                SQL.commandoSQL.Parameters.AddRange(pagosPacienteFinanzas);
+                using (var lector = SQL.commandoSQL.ExecuteReader())
+                {
+                    while (lector.Read())
+                    {
+                        Dictionary<string, object> Pago = new Dictionary<string, object>()
+                        {
+                            { "IdPago", int.Parse(lector["id"].ToString()) },
+                            { "FechaRegistro", lector["fechahora"].ToString() },
+                            { "Pago", double.Parse(lector["montopago"].ToString()) }
+                        };
+                        ListaPagos.Add(Pago);
+                    }
+                }
+
+                SQL.transaccionSQL.Commit();
+                return JsonConvert.SerializeObject(ListaPagos);
+            }
+            catch (Exception e)
+            {
+                SQL.transaccionSQL.Rollback();
+                return e.ToString();
+            }
+            finally
+            {
+                SQL.conSQL.Close();
+            }
+        }
+
+        // FUNCION QUE REALIZA EL PAGO DE UN PACIENTE
+        public string GenerarPagoPaciente(PacientePagos pacientepago, string tokenusuario)
+        {
+            try
+            {
+                SQL.comandoSQLTrans("PagoPaciente");
+                SQL.commandoSQL = new SqlCommand("INSERT INTO dbo.pacienteregistropagos (idusuario, idfinanzas, montopago, folrefdesc, fechahora, admusuario) VALUES ((SELECT id FROM dbo.usuarios WHERE tokenusuario = @TokenDATA), @IdFinanzasParam, @MontoPagoParam, @FolRefDescParam, @FechaParam, (SELECT usuario FROM dbo.usuarios WHERE tokenusuario = @TokenDATA))", SQL.conSQL, SQL.transaccionSQL);
+                SqlParameter[] pagoPaciente =
+                {
+                    new SqlParameter("@TokenDATA", SqlDbType.VarChar){Value = tokenusuario },
+                    new SqlParameter("@IdFinanzasParam", SqlDbType.Int){Value = pacientepago.IdFinanzas },
+                    new SqlParameter("@MontoPagoParam", SqlDbType.Float){Value = pacientepago.MontoPago },
+                    new SqlParameter("@FolRefDescParam", SqlDbType.VarChar){Value = pacientepago.FolRefDesc },
+                    new SqlParameter("@FechaParam", SqlDbType.DateTime){Value = MISC.FechaHoy() }
+                };
+                SQL.commandoSQL.Parameters.AddRange(pagoPaciente);
+                SQL.commandoSQL.ExecuteNonQuery();
+
+                SQL.commandoSQL = new SqlCommand("UPDATE dbo.pacienteregistro SET estatus = 2 WHERE id = (SELECT idpaciente FROM dbo.pacienteregistrofinanzas WHERE id = @IdFinanzasParam)", SQL.conSQL, SQL.transaccionSQL);
+                SQL.commandoSQL.Parameters.Add(new SqlParameter("@IdFinanzasParam", SqlDbType.Int) { Value = pacientepago.IdFinanzas });
                 SQL.commandoSQL.ExecuteNonQuery();
 
                 SQL.transaccionSQL.Commit();
